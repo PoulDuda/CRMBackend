b1d51b4276273ba5c057db696e068247652e9dc4
PoulDuda <dudinpasha@gmail.com>
2024-09-28 11:47:43 +0300

    add models

---
+++ Models/LoginDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRMAuth.Models;
+
+public class LoginDto
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Required]
+    public string Password { get; set; }
+}
+
+public class LoginResponseDto
+{
+    [Required]
+    public string JwtToken { get; set; }
+}
---
+++ Models/UserDto.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRMAuth.Models;
+
+public class UserDto
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string AvatarUrl { get; set; }
+}
+
+public class CreateUserDto
+{
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; }
+    [Required]
+    [EmailAddress]
+    [MaxLength(100)]
+    public string Email { get; set; }
+    [Required]
+    [MaxLength(450)]
+    public string Password { get; set; }
+    [MaxLength(450)]
+    public string AvatarUrl { get; set; }
+}
+
+public class UpdateUserDto
+{
+    [MaxLength(100)]
+    public string Name { get; set; }
+    [MaxLength(450)]
+    public string AvatarUrl { get; set; }
+    // [EmailAddress]
+    // [MaxLength(100)]
+    // public string Email { get; set; }
+    // [MaxLength(450)]
+    // public string Password { get; set; } // to add next time
+}
