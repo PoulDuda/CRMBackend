00047ca5a10cee63ecdc24e286da20800b0464b5
PoulDuda <dudinpasha@gmail.com>
2024-09-28 15:05:31 +0300

    add registration model

---
+++ Models/RegistrationDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRMAuth.Models;
+
+public class RegistrationDto
+{
+    [Required]
+    public int id { get; set; }
+    [Required]
+    public string name { get; set; }
+    [Required]
+    [EmailAddress]
+    public string email { get; set; }
+    [Required]
+    public string? avatar_url { get; set; }
+    [Required]
+    public DateTime created_at { get; set; } 
+}
