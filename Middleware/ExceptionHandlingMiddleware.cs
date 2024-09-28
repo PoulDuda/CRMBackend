97b41928d509d79d38c3b28def75548e1af8218f
PoulDuda <dudinpasha@gmail.com>
2024-09-28 11:49:31 +0300

    add middleware

---
+++ Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+
+namespace CRMAuth.Middleware;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _requestDelegate;
+
+    public ExceptionHandlingMiddleware(RequestDelegate requestDelegate)
+    {
+        _requestDelegate = requestDelegate;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _requestDelegate(context);
+        }
+        catch (Exception e)
+        {
+            await HandleExceptionAsync(context, e);
+        }
+    }
+
+    private Task HandleExceptionAsync(HttpContext context, Exception ex)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        var response = new
+        {
+            StatusCode = context.Response.StatusCode,
+            Message = "Server internal error. Please, try later!",
+            Detailed = ex.Message // Для отладки можно включить
+        };
+        return context.Response.WriteAsync(JsonConvert.SerializeObject(response));
+    } 
+}
